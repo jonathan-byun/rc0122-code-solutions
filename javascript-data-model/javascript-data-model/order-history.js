81c5f0cd021835a0df2707589fa95f4473995f5c
Jonathan Byun <jonathanbyun1@gmail.com>
2022-02-01 10:24:12 -0800

    Completed javascript-data-model exercise. Jonathan Byun

---
+++ javascript-data-model/javascript-data-model/order-history.js
@@ -0,0 +1,38 @@
+var orderHistory = [
+  {
+    items: 'JavaScript for impatient progammers',
+    total: 31.55,
+    shipTo: 'JS Masher',
+    placed: 'August 4, 2020',
+    delivered: 'Aug 8, 2020',
+    returnWindowEnd: 'Sep 7, 2020',
+    orderNumber: '#114-3941689-8772232'
+  },
+  {
+    items: 'The Timeless Way of Building',
+    total: 41.33,
+    shipTo: 'JS Masher',
+    placed: 'July 19, 2020',
+    delivered: 'Jul 20, 2020',
+    returnWindowEnd: 'Aug 19, 2020',
+    orderNumber: '#113-9984268-1280257'
+  },
+  {
+    items: 'Gamecube Controller Adapter. Super Smash Bros Switch Gamecub Adapter for WII U, PC. Support Turbo and Vibration Features. No Driver and No Lag-Gamecube Adapter',
+    total: 15.98,
+    shipTo: 'JS Masher',
+    placed: 'July 4, 2020',
+    delivered: 'Jul 7, 2020',
+    returnWindowEnd: 'Aug 5, 2020',
+    orderNumber: '#114-2875557-9059409'
+  },
+  {
+    items: 'GameCube Controller - Super Smash Bros. Edition (Nintendo Switch), The Art of Sql',
+    total: 138.93,
+    shipTo: 'JS Masher',
+    placed: 'July 3, 2020',
+    delivered: 'July 5, 2020',
+    returnWindowEnd: 'Aug 4, 2020',
+    orderNumber: '#113-2883177-2648248'
+  }
+];
