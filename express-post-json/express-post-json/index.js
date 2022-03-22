79de2f0298d5414e0948f72399876d1fc1bacde1
jonathan-byun <jonathanbyun1@gmail.com>
2022-03-22 11:31:34 -0700

    completed express-post-json exercise. Jonathan Byun

---
+++ express-post-json/express-post-json/index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+let nextID = 1;
+const grades = {};
+
+app.get('/api/grades', (req, res) => {
+  const returnArray = [];
+  for (const key in grades) {
+    returnArray.push(grades[key]);
+  }
+  res.json(returnArray);
+});
+
+app.use(express.json());
+
+app.post('/api/grades', (req, res) => {
+  req.body.Id = nextID;
+  grades[nextID] = req.body;
+  res.json(req.body);
+  nextID++;
+  res.status(204);
+});
+
+app.listen(3000, (req, res) => {
+
+});
